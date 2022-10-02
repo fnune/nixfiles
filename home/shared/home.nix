64ce3f53eca3c6a3ccb943f2b0df833601d77722
Fausto Núñez Alberro <fausto.nunez@mailbox.org>
2022-10-02 14:00:30 +0200

    Add (not yet merged) editorconfig suppoort

--- home/shared/home.nix
+++ home/shared/home.nix
@@ -1,7 +1,7 @@
 { config, lib, pkgs, ... }:
 
 {
-  imports = [ ./desktop/gnome.nix (import ./programs/zsh.nix { pkgs = pkgs; config = config; }) ./programs/git.nix (import ./programs/alacritty.nix { config = config; }) (import ./programs/neovim.nix { config = config; pkgs = pkgs; }) (import ./programs/tmux.nix { pkgs = pkgs; }) (import ./programs/lazygit.nix { pkgs = pkgs; }) ];
+  imports = [ ./desktop/gnome.nix (import ./programs/zsh.nix { pkgs = pkgs; config = config; }) ./programs/git.nix (import ./programs/alacritty.nix { config = config; }) (import ./programs/neovim.nix { config = config; pkgs = pkgs; }) (import ./programs/tmux.nix { pkgs = pkgs; }) (import ./programs/lazygit.nix { pkgs = pkgs; }) ./programs/editorconfig.nix ];
 
   home.packages = with pkgs; [ ripgrep wget unzip zip ];
   home.stateVersion = "22.05";
---
+++ home/shared/programs/editorconfig.nix
@@ -0,0 +1,15 @@
+{
+  # Support merged recently: https://github.com/nix-community/home-manager/pull/3204
+  # Not yet available on 22.05.
+  # editorconfig.enable = true;
+  # editorconfig.settings = {
+  #   "*" = {
+  #     charset = "utf-8";
+  #     end_of_line = "lf";
+  #     indent_size = 2;
+  #     indent_style = "space";
+  #     insert_final_newline = true;
+  #     trim_trailing_whitespace = true;
+  #   };
+  # };
+}
