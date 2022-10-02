2f24b0e414092f8155a2fccdc4a9857087402009
Fausto Núñez Alberro <fausto.nunez@mailbox.org>
2022-10-02 19:14:19 +0200

    Add what's in base-devel or build-essential

--- home/shared/home.nix
+++ home/shared/home.nix
@@ -3,7 +3,34 @@
 {
   imports = [ ./desktop/gnome.nix (import ./programs/zsh.nix { pkgs = pkgs; config = config; }) ./programs/git.nix (import ./programs/alacritty.nix { config = config; }) (import ./programs/neovim.nix { config = config; pkgs = pkgs; }) (import ./programs/tmux.nix { pkgs = pkgs; }) (import ./programs/lazygit.nix { pkgs = pkgs; }) ./programs/editorconfig.nix ];
 
-  home.packages = with pkgs; [ ripgrep wget unzip zip ];
+  home.packages = with pkgs; [
+    autoconf
+    automake
+    binutils
+    bison
+    elfutils
+    fakeroot
+    file
+    file
+    findutils
+    flex
+    gawk
+    gcc
+    gettext
+    gnumake
+    groff
+    gzip
+    libtool
+    m4
+    patch
+    pkgconf
+    ripgrep
+    texinfo
+    unzip
+    wget
+    which
+    zip
+  ];
   home.stateVersion = "22.05";
 
   home.username = "fausto";
